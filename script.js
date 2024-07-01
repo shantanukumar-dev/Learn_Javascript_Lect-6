21676232c8721f57c74db20292f7f75c7ec1cb74
Shantanu Kumar Mahanta <121172938+shantanukumar-dev@users.noreply.github.com>
2024-07-01 22:47:01 +0530

    Add files via upload
    This  is the 6th session of learning javascript. In this session we disscussed about the Mathematical operator
    Basically Mathematical operator are used to perform mathematicalExpression.
    1.ADDITION OPERATOR
    2.SUBTRACTION OPERATOR
    3.MULTIPLICATION OPERATOR
    4.DIVISON OPERATOR
    5.REMAINDER OPERATOR
    6.EXPONENTIATIONAL  OPERATOR

---
+++ script.js
@@ -0,0 +1,19 @@
+let n1=100;
+let n2=20;
+ let n3=n1+n2;
+ console.log(n3);
+
+ let n4=n1-n2;
+ console.log(n4);
+
+ let n5=n1*n2;
+ console.log(n5);
+
+ let n6=n1/n2;
+ console.log(n6);
+
+ let n7=n1%n2;
+ console.log(n7);
+ 
+ let n8=n1**n2;
+ console.log(n8);
